d9eafbc06eba7a69c8cb4ac6618f631b7bbfbd04
vibracode <codevibra@gmail.com>
2022-08-09 09:46:59 +0430

    tutorial project begin

---
+++ 3.tutorialproject/src/1.classes vs functions.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>function</div>
+  )
+}
+
+import React, { Component } from 'react'
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>class</div>
+    )
+  }
+}
+
+//functions are much better than classes unless we have to use classes sometimes
---
+++ 3.tutorialproject/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1 align="center">this is a react tutorial project</h1>
+		</div>
+	);
+}
+
+export default App;
