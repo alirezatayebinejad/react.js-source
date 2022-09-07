42132cb1b944a9b4df9a0332670c938549619fc1
vibracode <codevibra@gmail.com>
2022-09-07 18:19:33 +0430

    initialize redux store and codes

--- 4.reduxtutorial/1.tutorial.js
+++ 4.reduxtutorial/1.tutorial.js
@@ -1,3 +1,12 @@
+//npm install @reduxjs/toolkit react-redux
+
+//in app/store.js
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+	reducer: {},
+});
+//in index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -13,8 +22,3 @@ root.render(
 		</Provider>
 	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
