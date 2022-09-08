0e8a3f013425d6bad7a86be0ac5c0510a6eb6e31
vibracode <codevibra@gmail.com>
2022-09-08 17:19:16 +0430

    initial project for redux structure

--- 4.reduxtutorial/1.tutorial.js
+++ 4.reduxtutorial/1.tutorial.js
@@ -1,16 +1,18 @@
+//install redux
 //npm install @reduxjs/toolkit react-redux
 
+//a simple app for redux structure
 //in app/store.js
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		counter: counterReducer,
+	},
 });
+
 //in index.js
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
@@ -22,3 +24,67 @@ root.render(
 		</Provider>
 	</React.StrictMode>
 );
+
+//in features/counter/counterSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	count: 0,
+};
+
+export const counterSlice = createSlice({
+	name: "counter",
+	initialState,
+	reducers: {
+		increment: (state) => {
+			state.count += 1;
+		},
+		decrement: (state) => {
+			state.count -= 1;
+		},
+		reset: (state) => {
+			state.count = 0;
+		},
+		incrementByAmount: (state, action) => {
+			state.count += action.payload;
+		},
+	},
+});
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+//export default counterSlice.reducer;
+
+//in Counter component
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+
+function Counter() {
+	const count = useSelector((state) => state.counter.count);
+	const dispatch = useDispatch();
+
+	const [incrementAmount, setIncrementAmount] = useState(0);
+
+	const addValue = Number(incrementAmount) || 0;
+
+	const resetAll = () => {
+		setIncrementAmount(0);
+		dispatch(reset());
+	};
+
+	return (
+		<section>
+			<p>{count}</p>
+			<div>
+				<button onClick={() => dispatch(increment())}>+</button>
+				<button onClick={() => dispatch(decrement())}>-</button>
+			</div>
+			<input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+			<div>
+				<button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+				<button onClick={resetAll}>reset</button>
+			</div>
+		</section>
+	);
+}
+
+//export default Counter;
--- 4.reduxtutorial/src/App.js
+++ 4.reduxtutorial/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./features/counter/Counter";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>hello world lets learn redux🚀🔥</h1>
+			<Counter />
 		</div>
 	);
 }
